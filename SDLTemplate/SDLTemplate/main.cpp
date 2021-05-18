1f046c96f5aa5cc08607d090bfa3ed60745d9f97
kbvalmonte <kevinbrian.valmonte@benilde.edu.ph>
2021-05-18 14:39:52 +0800

    Added blank window

--- SDLTemplate/SDLTemplate/main.cpp
+++ SDLTemplate/SDLTemplate/main.cpp
@@ -1,8 +1,29 @@
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Video Initialization Error: " << SDL_GetError() << std::endl;
+	}
+	else
+	{
+		window = SDL_CreateWindow("BASPRG3 SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			std::cout << "Window creation error: " << SDL_GetError() << std::endl;
+		}
+		else
+		{
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
